f2708dffce69267c3065f6999025c09f86b2d8a4
Leonardo Santos <leonardo.santos@iotag.com.br>
2024-10-14 14:48:44 -0300

    playground funcionando

--- appChat.py
+++ appChat.py
@@ -1,13 +1,27 @@
 from ChatModel import *
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 from langserve import add_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Meu chat', description='chatboot para uso pessoal')
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
 
 add_routes(app, chain, path='/chat')
 
 if __name__ == "__main__":
     import uvicorn
+    import nest_asyncio
+    nest_asyncio.apply()
     uvicorn.run(app, host="0.0.0.0", port=9090)
-        
-    
